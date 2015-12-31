02c83e6134b70237b75f55732a74c67abb765e6f
Jacek <jacek@opcode.pl>
2015-12-31 10:01:33 +0100

    Initial commit

---
+++ src/RegexReplace.php
@@ -0,0 +1,60 @@
+<?php
+
+class RegexReplace
+{
+    /**
+     * @param $string
+     * @param array $data
+     * @return mixed
+     */
+    public function execute($string, array $data)
+    {
+        preg_match_all('/{.*?}/', $string, $ret);
+
+        foreach($ret as $variables) {
+            foreach($variables as $variable) {
+                $string = $this->parse($string, $data, $variable);
+            }
+        }
+
+        return $string;
+    }
+
+    /**
+     * @param $string
+     * @param array $data
+     * @param $variable
+     * @return mixed
+     */
+    private function parse($string, array $data, $variable)
+    {
+        $variableStripped = str_replace(array('{', '}'), '', $variable);
+        $variableData = explode(',', $variableStripped);
+
+        $variableName = null;
+        $variableFallback = null;
+
+        if (isset($variableData[0])) {
+            $variableName = $variableData[0];
+        }
+
+        if (isset($variableData[1])) {
+            $variableFallback = str_replace('fallback=', '', $variableData[1]);
+        }
+
+        if (isset($data[$variableName]) && null !== $data[$variableName]) {
+            $string = str_replace($variable, $data[$variableName], $string);
+
+            return $string;
+        }
+
+        if (isset($variableFallback)) {
+            $string = str_replace($variable, $variableFallback, $string);
+
+            return $string;
+        }
+
+        return $string;
+    }
+}
+
---
+++ tests/RegexReplaceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class RegexReplaceTest extends PHPUnit_Framework_TestCase
+{
+    public function testRegexReplace()
+    {
+        $rr = new RegexReplace();
+
+
+        $str = "Hello {name,fallback=Customer}, this is a message sent to {email}, we'll be in touch in writing at {address,fallback=Your Postal Address}";
+
+        $data1 = array(
+            'name' => "John Smith",
+            'email' => "john@google.com",
+            'address' => NULL,
+        );
+
+        $data2 = array(
+            'name' => NULL,
+            'email' => "steve@google.com",
+            'address' => "123 Berkshire Road, London",
+        );
+
+        $result1 = "Hello John Smith, this is a message sent to john@google.com, we'll be in touch in writing at Your Postal Address";
+        $result2 = "Hello Customer, this is a message sent to steve@google.com, we'll be in touch in writing at 123 Berkshire Road, London";
+
+        $this->assertEquals($result1, $rr->execute($str, $data1));
+        $this->assertEquals($result2, $rr->execute($str, $data2));
+    }
+}
